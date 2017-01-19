685245a96c757821e3ab15421e3123adfdc4b170
A. Iván Lozano R <angel_ivan_5@outlook.com>
2017-01-19 11:23:13 -0600

    mano v0.1 - InMoov Hand - AILR
    This version it's used to calibrate and try Servomotors with Arduino Boards.. 
    2016.
    Angel Iván Lozano Ramírez.
    https://goo.gl/rQB4H5

---
+++ mano_v0.1/mano_v0.1.ino
@@ -0,0 +1,33 @@
+/*
+ * Código para calibración y prueba de Servomotores
+ * Por Angel Iván Lozano Ramírez
+ * 2016
+ * https://github.com/ailr16/InMoov-Control---AILR
+ * https://www.youtube.com/channel/UCJ5MFm2qRBsL5Vf8Yft5vpw
+ */
+
+#include <Servo.h>            //Librería para control de servos
+
+Servo motor;                  //Creación de servo
+
+int pot = 0;                  //Pin A0 para leer valor analógico
+int angulo = 0;               //Valor tipo entero de 0 para ángulo
+int valor = 0;
+
+
+void setup() {
+  pinMode(pot, INPUT);        //Pin A0 como entrada
+  motor.attach(9);            //Servomotor en pin 9
+  Serial.begin(9600);         //Iniciar comunicación serie con PC
+}
+
+void loop() {
+  
+   valor = analogRead(A0);    //Asignar lectura analógica a entero valor
+   angulo  = map( valor, 0, 1024, 5, 180);      //Mapear límites y asignar a entero ángulo
+   motor.write(angulo);       //Escribir ángulo en servomotor
+   delay(1);                  //Retardo de 1ms
+   
+   Serial.println(valor);     //Imprimir valor en monitor serial
+
+}
